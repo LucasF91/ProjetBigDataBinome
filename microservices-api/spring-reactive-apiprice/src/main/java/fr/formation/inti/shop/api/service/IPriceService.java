3e3010653fc79ab36b20489802673f32cf08961d
Lucas Fontaine <lucasfontaine@orange.fr>
2020-04-22 09:44:37 +0200

    Modification du pom pour faire marcher le EndPoint

--- microservices-api/spring-reactive-apiprice/src/main/java/fr/formation/inti/shop/api/service/IPriceService.java
+++ microservices-api/spring-reactive-apiprice/src/main/java/fr/formation/inti/shop/api/service/IPriceService.java
@@ -1,7 +1,5 @@
 package fr.formation.inti.shop.api.service;
 
-import com.inti.formation.shop.api.repository.model.Customer;
-
 import fr.formation.inti.shop.api.repository.model.Price;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
